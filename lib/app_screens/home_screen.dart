6c0a6e7899255c3be3f3229faaf19ac80a8b52ef
Salma <syamh26701@gmail.com>
2022-09-25 13:36:07 +0200

    test

--- lib/app_screens/home_screen.dart
+++ lib/app_screens/home_screen.dart
@@ -10,7 +10,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.purple,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
